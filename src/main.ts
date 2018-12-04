83572856826bdaa2df8e61e516837d4fb338cd92
hi1280 <hi12801280@gmail.com>
2018-12-04 22:49:21 +0900

    add main function

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
-export function main(message: string) {
-  console.log(`${message}`);
-  return `Hello ${message}`;
+import axios from "axios";
+import { Qiita } from "./qiita";
+
+export async function main() {
+  const res = await axios.get<Qiita[]>("https://qiita.com/api/v2/items");
+  console.log(res.data);
+  return res;
 }
 
 if (require.main === module) {
-  main(process.argv[2]);
+  main();
 }
