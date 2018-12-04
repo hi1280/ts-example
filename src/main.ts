05c1507b42dbc110cf5d7511849a0e998765b99c
hi1280 <hi12801280@gmail.com>
2018-12-04 21:20:37 +0900

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+export function main(message: string) {
+  console.log(`${message}`);
+  return `Hello ${message}`;
+}
+
+if (require.main === module) {
+  main(process.argv[2]);
+}
