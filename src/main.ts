c9300b432f815373b24807c4fa4ed3fb3586117e
hi1280 <hi12801280@gmail.com>
2018-12-09 22:51:10 +0900

    add prettier and git pre-commit task

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-import axios from "axios";
-import { Qiita } from "./qiita";
+import axios from 'axios';
+import { Qiita } from './qiita';
 
 export async function main() {
-  const res = await axios.get<Qiita[]>("https://qiita.com/api/v2/items");
+  const res = await axios.get<Qiita[]>('https://qiita.com/api/v2/items');
   console.log(res.data);
   return res;
 }
