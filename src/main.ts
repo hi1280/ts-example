0ad9b51c9261f5e7ca7f6ae935c846d2a10f348e
hi1280 <hi12801280@gmail.com>
2018-12-14 23:52:35 +0900

    add elasticsearch

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,38 @@
 #!/usr/bin/env node
 import axios from 'axios';
+import elasticsearch from 'elasticsearch';
 import redis from 'redis';
 import { promisify } from 'util';
 import { Qiita } from './qiita';
 
+const esclient = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace',
+});
+
 export async function main() {
   const client = redis.createClient();
   const getAsync = promisify(client.get).bind(client);
-  let res = (await getAsync('items')) as any;
-  if (res) {
-    return JSON.parse(res);
+  const items = (await getAsync('items')) as any;
+  if (items) {
+    return JSON.parse(items);
   } else {
-    res = await axios.get<Qiita[]>('https://qiita.com/api/v2/items');
+    const res = await axios.get<Qiita[]>('https://qiita.com/api/v2/items');
     client.set('items', JSON.stringify(res.data), 'EX', 60);
+    const body: Array<{}> = [];
+    res.data.forEach(d => {
+      body.push({
+        index: {
+          _id: d.id,
+        },
+      });
+      body.push(d);
+    });
+    esclient.bulk({
+      body,
+      index: 'qiita',
+      type: 'items',
+    });
     return res.data;
   }
 }
